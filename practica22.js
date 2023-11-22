ccff33c6bba1c2b2c921152acb11f7e3572c1b94
danny <kdmunoz@sudamericano.edu.ec>
2023-11-22 17:38:50 -0500

    Initial commit

---
+++ practica22.js
@@ -0,0 +1,23 @@
+'use strict'
+
+let vehicle = {
+    make: 'Toyota',
+    model: 'Camry',
+    color: 'White',
+    licensePlate: 'ABC-0123',
+    year: 2009,
+    isEnable: false,
+
+}
+
+function validarTaxi(taxi) {
+    let antiguedad = new Date().getFullYear() - taxi.year;
+    if (antiguedad < 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+alert(validarTaxi(vehicle));
